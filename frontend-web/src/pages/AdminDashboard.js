6ec902d7b9129e87851c91dd54e2d07c756fd716
Ghryshvi <gispi.desu@gmail.com>
2026-01-11 18:18:00 +0700

    FIX: dashboard bug

--- frontend-web/src/pages/AdminDashboard.js
+++ frontend-web/src/pages/AdminDashboard.js
@@ -26,7 +26,7 @@ const AdminDashboard = () => {
         return;
       }
 
-      const response = await axios.get("http://localhost:8000/api/contacts", {
+      const response = await axios.get("/contacts", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
