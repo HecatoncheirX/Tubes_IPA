d99d13bee7e45603194994ce149cc498660ab875
Alam <aaallaaamm03@gmail.com>
2026-01-11 21:18:31 +0700

    TEST

--- frontend-web/src/utils/api.js
+++ frontend-web/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://31.220.18.132:8000/api",
+  baseURL: "http://localhost:8000/api",
 });
 
 api.interceptors.request.use(
@@ -12,6 +12,7 @@ api.interceptors.request.use(
     }
     return config;
   },
+  
   (error) => {
     return Promise.reject(error);
   }
